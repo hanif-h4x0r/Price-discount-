c8ba13d0d1b1158505666dca6d92ae202e8d429b
hanif0526 <hanifabdurrahmanarifai@gmail.com>
2026-05-01 15:09:12 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-def my_discount(price, discount):
-	price = float("Enter your price : ")
-	discount = float("Enter your discount : ")
-	if not isinstance(price, (int, float)) or not isinstance (discount, (int, float):
-		return "The price and discount should be a number"
-
-	return  price - (price * discount / 100)
-
-print(my_discount(price, discount))
+def my_discount():
+    try:
+        price = float(input("Enter your price : "))
+        discount = float(input("Enter your discount : "))
+        
+        if price <= 0:
+            return "The price should be a number"
+            
+        if discount  < 0 or discount > 100:
+            return "The discount should between 0 and 100"
+            
+        return price - (price * discount / 100)
+        
+    except ValueError:
+        return "Please enter a valid number"
+   
+print(my_discount())
